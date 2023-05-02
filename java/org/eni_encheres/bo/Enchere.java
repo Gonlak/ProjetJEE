83671d7b9124e44fbd3d5a1433fffc2204fe16db
Robin LAGADEC <robin.lagadec2023@campus-eni.fr>
2023-05-02 14:08:35 +0200

    Ajout de la branche devrobin

---
+++ java/org/eni_encheres/bo/Enchere.java
@@ -0,0 +1,59 @@
+package org.eni_encheres.bo;
+
+import java.sql.Date;
+
+public class Enchere {
+
+	//attributs
+	private int no_utilisateur;
+	private int no_article;
+	private Date date_enchere;
+	private int montant_enchere;
+	
+	//constructeurs
+	public Enchere() {}
+	
+	public Enchere(int no_utilisateur, int no_article, Date date_enchere, int montant_enchere) {
+		super();
+		this.no_utilisateur = no_utilisateur;
+		this.no_article = no_article;
+		this.date_enchere = date_enchere;
+		this.montant_enchere = montant_enchere;
+	}
+
+	//getters et setters
+	public int getNo_utilisateur() {
+		return no_utilisateur;
+	}
+
+	public void setNo_utilisateur(int no_utilisateur) {
+		this.no_utilisateur = no_utilisateur;
+	}
+
+	public int getNo_article() {
+		return no_article;
+	}
+
+	public void setNo_article(int no_article) {
+		this.no_article = no_article;
+	}
+
+	public Date getDate_enchere() {
+		return date_enchere;
+	}
+
+	public void setDate_enchere(Date date_enchere) {
+		this.date_enchere = date_enchere;
+	}
+
+	public int getMontant_enchere() {
+		return montant_enchere;
+	}
+
+	public void setMontant_enchere(int montant_enchere) {
+		this.montant_enchere = montant_enchere;
+	}
+	
+	
+	
+}
