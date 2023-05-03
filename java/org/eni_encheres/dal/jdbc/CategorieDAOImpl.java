d838bc8537db2bbd374cfd02c1a94fd8191004c3
Robin LAGADEC <robin.lagadec2023@campus-eni.fr>
2023-05-03 12:25:51 +0200

    Ajout UtilisateurDAOImpl,RetraitDAOImpl,CategorieDAOImpl

---
+++ java/org/eni_encheres/dal/jdbc/CategorieDAOImpl.java
@@ -0,0 +1,46 @@
+package org.eni_encheres.dal.jdbc;
+
+import java.util.List;
+
+import org.eni_encheres.bo.Categorie;
+import org.eni_encheres.dal.CategorieDAO;
+
+public class CategorieDAOImpl implements CategorieDAO{
+
+	@Override
+	public List<Categorie> selectByKeyWord(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Categorie> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Categorie categorie) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Categorie selectById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Categorie categorie) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ java/org/eni_encheres/dal/jdbc/RetraitDAOImpl.java
@@ -0,0 +1,46 @@
+package org.eni_encheres.dal.jdbc;
+
+import java.util.List;
+
+import org.eni_encheres.bo.Retrait;
+import org.eni_encheres.dal.RetraitDAO;
+
+public class RetraitDAOImpl implements RetraitDAO{
+
+	@Override
+	public List<Retrait> selectByKeyWord(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Retrait> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Retrait retrait) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Retrait selectById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Retrait retrait) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
