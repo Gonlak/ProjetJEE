60d23454d239448f1981470b966d14636afe38d8
Dimitri FONSAT <dfonsat2021@campus-eni.fr>
2023-05-03 11:28:24 +0200

    recuperation modif

--- java/org/eni_encheres/dal/DaoFactory.java
+++ java/org/eni_encheres/dal/DaoFactory.java
@@ -1,6 +1,30 @@
 package org.eni_encheres.dal;
 
-public abstract class DaoFactory<T> {
+public class DAOFactory {
 
+	public static ArticleVenduDAO getArticleVenduDAO() {
+		//return new articleVenduDAOImpl();
+		return null;
+	}
+	
+	public static CategorieDAO getCategorieDAO() {
+		//return new categorieDAOImpl();
+		return null;
+	}
+	
+	public static EncheresDAO enchereDAO() {
+		//return new enchereDAOImpl();
+		return null;
+	}
+	
+	public static RetraitDAO retraitDAO() {
+		//return new retraitDAOImpl();
+		return null;
+	}
+	
+	public static UtilisateurDAO utilisateurDAO() {
+		//return new utilisateurDAOImpl();
+		return null;
+	}
 	
 }
