862c4de7c3d78fe744aba199a68c3d34ebf9a114
Seb <sebc118@gmail.com>
2023-05-04 16:04:26 +0200

    modif home.jsp et de la Servlet pour afficher ou non le header

--- java/org/eni_encheres/ihm/HomeServlet.java
+++ java/org/eni_encheres/ihm/HomeServlet.java
@@ -5,6 +5,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.eni_encheres.bo.Utilisateur;
+
 import java.io.IOException;
 
 @WebServlet("")
@@ -13,6 +16,15 @@ public class HomeServlet extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		boolean sessionUtilisateur = false;
+
+		HttpSession session = request.getSession();
+		Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+		if(utilisateur!=null){
+			sessionUtilisateur = true;
+		}
+		request.setAttribute("sessionUtilisateur", sessionUtilisateur);
+
 		request.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(request, response);
 	}
 
