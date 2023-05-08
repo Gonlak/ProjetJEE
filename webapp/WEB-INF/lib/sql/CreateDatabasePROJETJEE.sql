d855559d51e82a18bbae41ffd7e68ddd377b044f
Seb <sebc118@gmail.com>
2023-05-08 15:02:34 +0200

    Modif CreateDatabasePROJETJEE.sql

--- webapp/WEB-INF/lib/sql/CreateDatabasePROJETJEE.sql
+++ webapp/WEB-INF/lib/sql/CreateDatabasePROJETJEE.sql
@@ -68,12 +68,12 @@ CREATE TABLE UTILISATEURS (
     pseudo           VARCHAR(30) NOT NULL,
     nom              VARCHAR(30) NOT NULL,
     prenom           VARCHAR(30) NOT NULL,
-    email            VARCHAR(20) NOT NULL,
+    email            VARCHAR(40) NOT NULL,
     telephone        VARCHAR(15),
     rue              VARCHAR(30) NOT NULL,
     code_postal      VARCHAR(10) NOT NULL,
     ville            VARCHAR(30) NOT NULL,
-    mot_de_passe     VARCHAR(30) NOT NULL,
+    mot_de_passe     VARCHAR(255) NOT NULL,
     credit           INTEGER NOT NULL,
     administrateur   bit NOT NULL
 )
